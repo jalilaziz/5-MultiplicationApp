3cb69eae6d3239a4005b7b8e7473e7a9cd22532c
jalilaziz <jaliilaziiz@gmail.com>
2024-01-01 20:10:35 +0100

    add button and changing css

--- script.js
+++ script.js
@@ -1,30 +1,73 @@
-function result() {
-    let inputNumber = document.getElementById('inputNumber'); 
-    let numSelect = document.getElementById('select'); 
-    let result = document.getElementById('result'); 
-    let calculationResult = parseInt(inputNumber.value) * parseInt(numSelect.value);
+// function result() {
+//     let inputNumber = document.getElementById('inputNumber'); 
+//     let numSelect = document.getElementById('select'); 
+//     let result = document.getElementById('result'); 
+//     let calculationResult = parseInt(inputNumber.value) * parseInt(numSelect.value);
 
-    result.innerHTML = "Result: " + inputNumber.value + " * " + numSelect.value + " = " + calculationResult;
+//     result.innerHTML = "Result: " + inputNumber.value + " * " + numSelect.value + " = " + calculationResult;
 
-    let resultText = "Result: " + inputNumber.value + " * " + numSelect.value + " = " + calculationResult;
+//     let resultText = "Result: " + inputNumber.value + " * " + numSelect.value + " = " + calculationResult;
 
-    document.getElementById("history").innerHTML += "<br>" + resultText;
-    localStorage.setItem("history", document.getElementById("history").innerHTML);
-}
+//     document.getElementById("history").innerHTML += "<br>" + resultText;
+//     localStorage.setItem("history", document.getElementById("history").innerHTML);
+// }
 
-window.onload = function () {
-    let savedHistory = localStorage.getItem("history");
-    if (savedHistory) {
-        document.getElementById("history").innerHTML = savedHistory;
+// window.onload = function () {
+//     let savedHistory = localStorage.getItem("history");
+//     if (savedHistory) {
+//         document.getElementById("history").innerHTML = savedHistory;
+//     }
+// };
+
+// function clearHistory() {
+//     document.getElementById("history").innerHTML = "";
+//     localStorage.removeItem("calculationHistory");
+// }
+
+// ***************************************
+
+let input = document.getElementById("inputNumber"); 
+let numSelect = document.getElementById("select");
+let result = document.getElementById("result");
+let history = document.getElementById("history"); 
+let showBtn = document.getElementById("btn"); 
+
+let obj={}
+
+if (localStorage.length>0) {
+    let localStorageData = JSON.parse(localStorage.getItem("data"))
+    obj={...localStorageData}
+
+    for (let n in obj) {
+        history.innerHTML += `<br>${n} = ${obj[n]}` 
     }
-};
-
-function clearHistory() {
-    document.getElementById("history").innerHTML = "";
-    localStorage.removeItem("calculationHistory");
 }
+
+showBtn.addEventListener("click", () => {
+    if (input.value !== "" && numSelect.value !== "") {
+        let r =  +input.value * +numSelect.value
+        result.innerHTML = "Result: " + r
+        obj[`${input.value} * ${numSelect.value}`]=r
+        localStorage.setItem("data", JSON.stringify(obj))
+        history.innerHTML += `<br>${input.value} * ${numSelect.value} = ${r}`
+        
+    } else {
+        alert("please enter a number")
+    }
+})
+
+function clearSession() {
+    document.getElementById("history").innerHTML = "";
+    localStorage.removeItem("history.innerHTML");
+}
+function clearData() {
+    document.getElementById("history").innerHTML = "";
+    localStorage.removeItem("data");
+}
+
 
 // **************************************
+
 
 // function result() {
 //     let inpuNummber = document.getElementById('inputNumber'); 
@@ -37,34 +80,3 @@ function clearHistory() {
 // document.getElementById("history").innerHTML += "<br>" + document.getElementById('result').innerHTML;
 // }
 
-// ***************************************
-
-// let input = document.getElementById("inputNumber"); 
-// let numSelect = document.getElementById("select");
-// let result = document.getElementById("result");
-// let history = document.getElementById("history"); 
-// let showBtn = document.getElementById("btn"); 
-
-// let obj={}
-
-// if (localStorage.length>0) {
-//     let localStorageData = JSON.parse(localStorage.getItem("data"))
-//     obj={...localStorageData}
-
-//     for (let n in obj) {
-//         history.innerHTML += `<br>${n} = ${obj[n]}` 
-//     }
-// }
-
-// showBtn.addEventListener("click", () => {
-//     if (input.value !== "" && numSelect.value !== "") {
-//         let r = +input.value * +numSelect.value
-//         result.innerHTML = r
-//         obj[`${input.value} * ${numSelect.value}`]=r
-//         localStorage.setItem("data", JSON.stringify(obj))
-//         history.innerHTML += `<br>${input.value} * ${numSelect.value} = ${r}`
-        
-//     } else {
-//         alert("please enter a number")
-//     }
-// })
